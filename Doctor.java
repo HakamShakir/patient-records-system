d1fe8882b7a1ade0f61709785d8d35e7694965ba
Alan Salam <92518949+AlanSalamm55@users.noreply.github.com>
2022-10-18 22:24:56 +0300

    added username,password and phoneno

--- Doctor.java
+++ Doctor.java
@@ -1,59 +1,43 @@
-
 import java.util.Objects;
 
-public class Doctor extends User {
+public class Doctor extends User{
 
-	private int yearsOfExperience;
-	private String qualifications, speciality;
+	private int  yearsOfExperience;
+	private String  speciality;
 
+	
+	
 	public int getYearsOfExperience() {
 		return yearsOfExperience;
 	}
 
-	public Doctor(String name, Gender gender, Position position, int age, int id, int salary, int yearsOfExperience,
-			String qualifications, String speciality) {
-		super(name, gender, position, age, id, salary);
+	public Doctor(String username,String password,String name,String phoneno ,Gender gender, Position position, int age, int id, int salary, int yearsOfExperience, String speciality) {
+		super(username ,password,name, phoneno,gender, position, age, id, salary);
 		this.yearsOfExperience = yearsOfExperience;
-		this.qualifications = qualifications;
 		this.speciality = speciality;
 	}
-
 	public void setYearsOfExperience(int yearsOfExperience) {
 		this.yearsOfExperience = yearsOfExperience;
-	}
-
-	public String getQualifications() {
-		return qualifications;
-	}
-
-	public void setQualifications(String qualifications) {
-		this.qualifications = qualifications;
 	}
 
 	public String getSpeciality() {
 		return speciality;
 	}
-
 	public void setSpeciality(String speciality) {
 		this.speciality = speciality;
 	}
 
 	@Override
 	public String toString() {
-
-		return super.toString() + "Doctor [yearsOfExperience=" + yearsOfExperience + ", qualifications="
-				+ qualifications + ", speciality="
-				+ speciality + "]";
+		return 	"Doctor" +super.toString()+"yearsOfExperience=" + yearsOfExperience + ", speciality="+ speciality + "]";
 	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
-		result = prime * result + Objects.hash(qualifications, speciality, yearsOfExperience);
+		result = prime * result + Objects.hash( speciality, yearsOfExperience);
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -63,8 +47,9 @@ public class Doctor extends User {
 		if (getClass() != obj.getClass())
 			return false;
 		Doctor other = (Doctor) obj;
-		return Objects.equals(qualifications, other.qualifications) && Objects.equals(speciality, other.speciality)
+		return   Objects.equals(speciality, other.speciality)
 				&& yearsOfExperience == other.yearsOfExperience;
 	}
-
+	
 }
+
