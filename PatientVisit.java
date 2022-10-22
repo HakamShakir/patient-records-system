ddbc6193ed13f54700e958742ad930785c001efe
Banel02 <115465070+Banel02@users.noreply.github.com>
2022-10-22 19:53:39 +0300

    Update PatientVisit.java

--- PatientVisit.java
+++ PatientVisit.java
@@ -2,7 +2,7 @@ package SystemForPatients;
 
 import java.time.LocalDate;
 
-public record PatientVisit(PatientRecord pr,int drId ,String chiefComplaint, String history, String vitals,
+public record PatientVisit(int drId ,String chiefComplaint, String history, String vitals,
 		String investigation, String treatement, String recommendation,LocalDate DateOfSubmission ,String duration, double price) {
 
 }
