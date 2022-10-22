2c197b045c2bb66814af9d8831492c9701f1d2e3
HakamShakir <hakamshakir2002@gmail.com>
2022-10-22 19:26:42 +0300

    3- Containers and data holders

--- Doctor.java
+++ Doctor.java
@@ -1,21 +1,32 @@
+package SystemForPatients;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
-public class Doctor extends User{
+public class Doctor extends User {
 
-	private int  yearsOfExperience;
-	private String  speciality;
+	private int yearsOfExperience;
+	private String speciality;
+	private String patientName;
+	private Scanner in = new Scanner(System.in);
+	LocalDate a = LocalDate.now();
 
-	
-	
+	public Doctor(String username, String password, String name, String phoneno, Gender gender, Position position,
+			int age, int id, int salary, int yearsOfExperience, String speciality) {
+		super(username, password, name, phoneno, gender, position, age, id, salary);
+		this.yearsOfExperience = yearsOfExperience;
+		this.speciality = speciality;
+	}
+
 	public int getYearsOfExperience() {
 		return yearsOfExperience;
 	}
 
-	public Doctor(String username,String password,String name,String phoneno ,Gender gender, Position position, int age, int id, int salary, int yearsOfExperience, String speciality) {
-		super(username ,password,name, phoneno,gender, position, age, id, salary);
-		this.yearsOfExperience = yearsOfExperience;
-		this.speciality = speciality;
-	}
 	public void setYearsOfExperience(int yearsOfExperience) {
 		this.yearsOfExperience = yearsOfExperience;
 	}
@@ -23,21 +34,25 @@ public class Doctor extends User{
 	public String getSpeciality() {
 		return speciality;
 	}
+
 	public void setSpeciality(String speciality) {
 		this.speciality = speciality;
 	}
 
 	@Override
 	public String toString() {
-		return 	"Doctor" +super.toString()+"yearsOfExperience=" + yearsOfExperience + ", speciality="+ speciality + "]";
+		return "Doctor" + super.toString() + "yearsOfExperience=" + yearsOfExperience + ", speciality=" + speciality
+				+ "]";
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
-		result = prime * result + Objects.hash( speciality, yearsOfExperience);
+		result = prime * result + Objects.hash(speciality, yearsOfExperience);
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -47,9 +62,65 @@ public class Doctor extends User{
 		if (getClass() != obj.getClass())
 			return false;
 		Doctor other = (Doctor) obj;
-		return   Objects.equals(speciality, other.speciality)
+		return Objects.equals(speciality, other.speciality)
 				&& yearsOfExperience == other.yearsOfExperience;
 	}
-	
-}
 
+	public void enterPatientName() {
+		System.out.println("enter the name of the patient:");
+		patientName = in.next();
+	}
+
+	// Searches for patient by name from a record given as argument.
+	public Optional<List<PatientRecord>> searchByName(List<PatientRecord> records) {
+
+		List<PatientRecord> d = records.stream().filter(p -> p.name().equals(patientName)).collect(Collectors.toList());
+
+		return Optional.ofNullable(d);
+
+	}
+
+	// Gets patient name from enterPatientName() and adds new visit to Map.
+	public void addPatientsVisits(Map<String, PatientVisit> visits) {
+
+		enterPatientName();
+
+		System.out.println("enter drId ID:");
+		int drId = in.nextInt();
+		System.out.println("enter cheif complaint: ");
+		String chiefComplaint = in.next();
+		System.out.println("enter history of the patient: ");
+		String history = in.next();
+		System.out.println("enter the vitals: ");
+		String vitals = in.next();
+		System.out.println("enter the investigation: ");
+		String investigation = in.next();
+		System.out.println("enter treatments: ");
+		String treatments = in.next();
+		System.out.println("enter recommendation: ");
+		String recommendation = in.next();
+		System.out.println("enter duration: ");
+		String duration = in.next();
+		System.out.println("enter the price: ");
+		double price = in.nextDouble();
+
+		visits.put(patientName, new PatientVisit(drId, chiefComplaint, history, vitals, investigation, treatments,
+				recommendation, a, duration, price));
+
+	}
+
+	public void viewPatientsData(List<PatientRecord> p) {
+
+		List<PatientRecord> DrsPatients = p.stream().filter(d -> d.drId() == this.getId()).collect(Collectors.toList());
+
+		DrsPatients.forEach(System.out::println);
+
+	}
+
+	public PatientVisit showVisits(Map<String, PatientVisit> visits) {
+		enterPatientName();
+		return visits.get(patientName);
+
+	}
+
+}
--- Main.java
+++ Main.java
@@ -1,10 +1,30 @@
-
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
-	public static void main(String [] args) {
-		
-		Doctor et = new Doctor("hasansabah","password","hasan salah","07501234567",Gender.FEMALE, Position.DENTIST,48,1,1600,20,"maxillofacial surgeon");
+	public static void main(String[] args) {
+
+		LocalDate a = LocalDate.now();
+
+		List<PatientRecord> Patients = new ArrayList<>();
+		Map<String, PatientVisit> visits = new HashMap<String, PatientVisit>();
+
+		Doctor et = new Doctor("hasansabah", "password", "hasan salah", "07501234567", Gender.FEMALE, Position.DENTIST,
+				48, 1, 1600, 20, "maxillofacial surgeon");
+		Receptionist Alan = new Receptionist("alansalam", "hello", "Alan", "023041340", Gender.MALE, Position.INTERN,
+				23, 0014, 2300);
+		PatientRecord Hakam = new PatientRecord("alan", "salam", Gender.MALE, a, "aaaa", 2);
+		Patients.add(Hakam);
+
+		et.addPatientsVisits(visits);
+		Patients.forEach(System.out::println);
+		System.out.println(Alan);
+
 		System.out.println(et.toString());
+
 	}
 }
