52baafd0680cb06237a4d85b1d43806207ac37fc
Alan Salam <92518949+AlanSalamm55@users.noreply.github.com>
2022-10-29 20:34:48 +0300

    added the serial number

--- Receptionist.java
+++ Receptionist.java
@@ -1,16 +1,12 @@
 package SystemForPatients;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
+
 
 public class Receptionist extends User{
-	private LocalDate d = LocalDate.now();
-	private Scanner in = new Scanner(System.in);
-	private String patientName;
 	
+
+	private static final long serialVersionUID = 8904255995808065269L;
+
 	public Receptionist(String username,String password,String name,String phoneno, Gender gender, Position position, int age, int id, int salary) {
 		super(username,password,name,phoneno, gender, position, age, id, salary);
 		
@@ -24,7 +20,6 @@ public class Receptionist extends User{
 		System.out.println(getPosition());
 		System.out.println(getAge());
 		System.out.println(getSalary());
-		
 		System.out.println(getId());
 	}
 	
@@ -45,64 +40,8 @@ public class Receptionist extends User{
 		return true;
 	}
 
-	public void getPatientInfo() {
-		
-	}
-	
-	public void addPatient(List<PatientRecord> patientRecord) {
-		
-		System.out.println("enter the patient info's");
-		
-		System.out.println("enter the patient Id:");
-		int patientId = in.nextInt();
-		
-		System.out.println("name: " );
-		String name = in.next();
-		
-		System.out.println("surName: ");
-		String surName = in.next();
-		 
-	    System.out.println("DR ID: ");
-	    int drId = in.nextInt();
-	
-	    System.out.println("phone Number:");
-	    String phoneNum = in.next();
-	    
-	    System.out.println("enter DOB in M/d/yyyy format: ");
-	    DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("M/d/yyyy");
-	    String userInput = in.next();
-		LocalDate DOB = LocalDate.parse(userInput , dateFormat);
-		
-		System.out.println("enter gender:");
-		String g = in.next();
-		Gender gender = Gender.valueOf(g.toUpperCase());
-		
-		
-		patientRecord.add(new PatientRecord(name, surName, gender, DOB, phoneNum, patientId,drId));
-	   
-		
-		
-	}
-	
-	public void enterPatientName() {
-		System.out.println("enter the name of the patient:");
-		patientName = in.next();
-	}
-	
-	public void viewRecords(List<Object>  records){
-	
-		records.stream().forEach(System.out:: println);
-		
-	}
-	public void printSearching(Optional<List<Object>> o) {
-		
-		if(!o.isEmpty()) {
-		System.out.println(o.toString());
-		}else {
-			o.get();
-		}
-	
+
 	
 }
 	
-}
+
