4da022e053c3bb2eb28c708bcce747cbc64ae801
Banel02 <115465070+Banel02@users.noreply.github.com>
2022-11-19 20:17:17 +0300

    Update Receptionists.java

--- Containers/Receptionists.java
+++ Containers/Receptionists.java
@@ -15,46 +15,42 @@ public class Receptionists implements Serializable {
 	private static final long serialVersionUID = 4L;
 	private List<Receptionist> receptionists;
 	
-//	private Scanner in = new Scanner(System.in);
-	
-//	public void enterId() {
-	//	System.out.println("enter the id that you wanna perform an operation on it: ");
-		//ids = in.nextInt();
-	//}
-	
-//	public void enterreceptionistName() {
-	//	System.out.println("enter the name of the receptionist:");
-		//receptionistName = in.next();
-	//}
 
     public Receptionists(List<Receptionist> receptionists) {
         this.receptionists = receptionists;
     }
 
     public void addReceptionist(String username,String password,String name,String phoneno, Gender gender,  int age, int id, int salary) throws Exception  {
-    	
+		List<Receptionist> receptionists=(List<Receptionist>) ResourceManager.load("files\\Receptionistslist.ser");
     	receptionists.add(new Receptionist(username, password, name, phoneno, gender, age, id, salary));
-    	
-		
+    	ResourceManager.save((Serializable) receptionists,"files\\Receptionistslist.ser");
 		
     }
 	
-	public Optional<List<Receptionist>> searchReceptionistByName(String receptionistName){
+	public Optional<List<Receptionist>> searchReceptionistByName(String receptionistName) throws Exception{
+		List<Receptionist> receptionists=(List<Receptionist>) ResourceManager.load("files\\Receptionistslist.ser");
+
 	     List<Receptionist> r = receptionists.stream().filter(receptionist -> receptionist.getName().equals(receptionistName)).collect(Collectors.toList());
 		return Optional.ofNullable(r);
 	}
 	
-	public void removeReceptionistById(int id) {
+	public void removeReceptionistById(int id) throws Exception {
+		List<Receptionist> receptionists=(List<Receptionist>) ResourceManager.load("files\\Receptionistslist.ser");
+
 		receptionists.removeIf(r -> r.getId() == id);
 										 }
 	
-    public List<String> getReceptionistNames() {
+    public List<String> getReceptionistNames() throws Exception {
+		List<Receptionist> receptionists=(List<Receptionist>) ResourceManager.load("files\\Receptionistslist.ser");
+
         List<String> receptionistNames = new ArrayList<>();
         receptionists.forEach(receptionist -> receptionistNames.add(receptionist.getName()));
         return receptionistNames;
     }
 
-	public void viewReceptionists(){
+	public void viewReceptionists() throws Exception{
+		List<Receptionist> receptionists=(List<Receptionist>) ResourceManager.load("files\\Receptionistslist.ser");
+
 		if(!receptionists.isEmpty()) {
 		receptionists.forEach(System.out:: println);
 		}else{
