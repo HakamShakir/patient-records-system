751ace25b8b360468fbc99b1750b1fb30e04faf7
Alan Salam <92518949+AlanSalamm55@users.noreply.github.com>
2022-11-19 18:12:23 +0300

    added methods with PrintWriter

--- Containers/PatientRecords.java
+++ Containers/PatientRecords.java
@@ -1,5 +1,6 @@
 package creating_views;
 
+import java.io.PrintWriter;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -18,7 +19,6 @@ public class PatientRecords implements Serializable  {
 	private static final long serialVersionUID = 3L;
 	private List<PatientRecord> patientrecords;
 	
-	//private Scanner in = new Scanner(System.in);
 	
 	private boolean add;
 	
@@ -28,73 +28,55 @@ public class PatientRecords implements Serializable  {
     }
 	
    public void addPatientRecord(String name, String surName, Gender male, LocalDate dOB, String phoneno,int patientId, int drId ,int age) throws Exception  {
-    	
+		List<PatientRecord> patientrecords =  (List<PatientRecord>) ResourceManager.load("files\\patientrecordss.ser");	
     	patientrecords.add(new PatientRecord( name, surName, male, dOB, phoneno , patientId, drId , age));
+		ResourceManager.save((Serializable) patientrecords, "files\\patientrecordss.ser"); 
+
     	
    }
     
-//	public void enterId() {
-//		System.out.println("enter the id that you wanna perform an operation on it: ");
-//		ids = in.nextInt();
-//	}
-	
-//	public void enterPatientName() {
-//		System.out.println("enter the name of the patient:");
-//		patientName = in.next();
-//	}
 
-
- /*   void addpatientRecrod()  {
-    	System.out.println("enter the patient info's");
-		System.out.println("enter the patient Id:");
-		int patientId = in.nextInt();
-		System.out.println("name: " );
-		String name = in.next();
-		System.out.println("surName: ");
-		String surName = in.next();
-	    System.out.println("DR ID: ");
-	    int drId = in.nextInt();
-	    System.out.println("phone Number:");
-	    String phoneNum = in.next();
-	    System.out.println("enter DOB in M/d/yyyy format: ");
-	    DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("M/d/yyyy");
-	    String userInput = in.next();
-		LocalDate DOB = LocalDate.parse(userInput , dateFormat);
-		System.out.println("enter gender:");
-		String g = in.next();
-		Gender gender = Gender.valueOf(g.toUpperCase());
-		patientrecords.add(new PatientRecord(name, surName, gender, DOB, phoneNum, patientId,drId));
-    }
-    
-    */
 	
-	public Optional<List<PatientRecord>> searchPatientRecordByName(String patientName){
+	public Optional<List<PatientRecord>> searchPatientRecordByName(String patientName) throws Exception{
+		List<PatientRecord> patientrecords =  (List<PatientRecord>) ResourceManager.load("files\\patientrecordss.ser");	
+
 	     List<PatientRecord> d = patientrecords.stream().filter(pt -> pt.name().equals(patientName)).collect(Collectors.toList());
 	     return Optional.ofNullable(d);
 		
 	}
-	public Optional<List<PatientRecord>> searchPatientRecordBydrId(int drId){
+	public Optional<List<PatientRecord>> searchPatientRecordBydrId(int drId) throws Exception{
+		List<PatientRecord> patientrecords =  (List<PatientRecord>) ResourceManager.load("files\\patientrecordss.ser");	
+
 	     List<PatientRecord> d = patientrecords.stream().filter(pt -> pt.drId() == drId).collect(Collectors.toList());
 	     return Optional.ofNullable(d);
 		
 	}
 	
-	public boolean isThePatientExistById(int pId) {
+	public boolean isThePatientExistById(int pId) throws Exception {
+		List<PatientRecord> patientrecords =  (List<PatientRecord>) ResourceManager.load("files\\patientrecordss.ser");	
+
 		 return patientrecords.stream().anyMatch(p -> p.patientId() == pId);
 	}
 	
-	public void removePatientRecordById(int ids) {
+	public void removePatientRecordById(int ids) throws Exception {
+		List<PatientRecord> patientrecords =  (List<PatientRecord>) ResourceManager.load("files\\patientrecordss.ser");	
+
 		patientrecords.stream().filter(p -> p.patientId() == ids).collect(Collectors.toList()).removeAll(patientrecords);
-												}
+		ResourceManager.save((Serializable) patientrecords, "files\\patientrecordss.ser"); 
+
+				}
 	
-    public List<String> getPatientNames() {
+    public List<String> getPatientNames() throws Exception {
+		List<PatientRecord> patientrecords =  (List<PatientRecord>) ResourceManager.load("files\\patientrecordss.ser");	
+
         List<String> PatientNames = new ArrayList<>();
         patientrecords.forEach(p -> PatientNames.add(p.name()));
         return PatientNames;
     }
     
-	public void viewRecords(){
+	public void viewRecords() throws Exception{
 		
+		List<PatientRecord> patientrecords =  (List<PatientRecord>) ResourceManager.load("files\\patientrecordss.ser");	
 		patientrecords.forEach(System.out:: println);
 	
 	}
@@ -115,6 +97,20 @@ public class PatientRecords implements Serializable  {
 			o.get();
 		}
 		}
+
+	public void viewRecords(PrintWriter out) throws Exception {
+		List<PatientRecord> patientrecords =  (List<PatientRecord>) ResourceManager.load("files\\patientrecordss.ser");	
+		patientrecords.forEach(out:: println);
+		
+	}
+
+	public void printSearching(Optional<List<PatientRecord>> o, PrintWriter out) {
+		if(!o.isEmpty()) {
+			out.println(o.toString());
+			}else {
+				o.get();
+			}		
+	}
 
 	
 }
