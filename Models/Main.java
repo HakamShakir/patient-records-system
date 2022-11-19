2c5b929752052fd4c87d897546ada307d586422f
Banel02 <115465070+Banel02@users.noreply.github.com>
2022-11-19 21:27:54 +0300

    Add files via upload

---
+++ Models/Main.java
@@ -0,0 +1,24 @@
+package creating_views;
+ import ClientAndServer.*;
+import View.Lists;
+import View.LogIn;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server();
+
+        server.startConnection();
+
+
+    }
+
+}
